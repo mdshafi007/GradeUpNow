a8424da22f5c8dcbc0fb2469230b2be378e63980
mdshafi007 <shaikmohammadshafi177@gmail.com>
2025-01-09 22:15:15 +0530

    Footer_Compo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
-import HeroSection from "./components/herosection/herosection";
+import HeroSection from "./components/herosection/Herosection";
 import Featuredtutorials from "./components/toptutorials/Featuredtutorials";
 import WhyGradeUpNow from "./components/services/WhyGradeUpNow";
 import Footer from "./components/Footer/Footer";
