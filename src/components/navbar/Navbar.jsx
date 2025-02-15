c8e2ef19e669041c32a40d1002947540da31cbb2
mdshafi007 <shaikmohammadshafi177@gmail.com>
2025-02-15 23:53:11 +0530

    navbar_close_after_click_fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,24 +4,30 @@ import "./navbar.css";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
     <header className="header">
       <a href="/" className="logo">GradeUpNow</a>
       <nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
-        
-        <Link to="/courses" className="coursei">Courses</Link>
-        <Link to="/notes" className="a">Notes</Link>
-        <a href="" className="a">Notifications</a>
-        <Link to="/login" className="loginn">Login</Link>
-        <Link to="/signup" className="signup">Signup</Link>
-        
+        <Link to="/courses" className="coursei" onClick={closeMenu}>Courses</Link>
+        <Link to="/notes" className="a" onClick={closeMenu}>Notes</Link>
+        <a href="" className="a" onClick={closeMenu}>Notifications</a>
+        <Link to="/login" className="loginn" onClick={closeMenu}>Login</Link>
+        <Link to="/signup" className="signup" onClick={closeMenu}>Signup</Link>
       </nav>
-      <button className={`hamburger ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+      <button 
+        className={`hamburger ${isMenuOpen ? "active" : ""}`} 
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
         <span className="line"></span>
         <span className="line"></span>
         <span className="line"></span>
